1ecbbec6a752b9c843f36daa62a8d285adf80a03
Talha Rehman <talharehman7o@gmail.com>
2021-05-06 18:34:13 +0500

    basic test demo

--- App.tsx
+++ App.tsx
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button } from "native-base";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Dimensions } from "react-native";
+import ConfettiCannon from "react-native-confetti-cannon";
 
 export default function App() {
+  const windoWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
+  const [top, setTop]: any = useState(0);
+  const [left, setLeft]: any = useState(0);
+  const [score, setScore]: any = useState(0);
+
+ let timer:any;
+  useEffect(() => {
+
+       timer = setInterval(() => {
+        setTop(Math.floor(Math.random() * windowHeight));
+        setLeft(Math.floor(Math.random() * windoWidth));
+        setScore(score - 1);
+      }, 3000);
+    return () => clearInterval(timer);
+  }, []);
+  useEffect(() => {
+    if (score == 5) {
+      alert("You Win!");
+      clearInterval(timer);
+    } else if (score == -5) {
+      alert("You Lose!");
+      clearInterval(timer);
+    }
+  }, [score]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {score >4 && <ConfettiCannon count={200} origin={{ x: -10, y: 0 }} />}
+      <Text>Score : {score}</Text>
+      <Button
+        style={{
+          position: "absolute",
+          top: top,
+          left: left,
+          padding: 10,
+        }}
+        onPress={() => setScore(score + 1)}
+      >
+        <Text>Catch me</Text>
+      </Button>
     </View>
   );
 }
@@ -14,8 +53,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
